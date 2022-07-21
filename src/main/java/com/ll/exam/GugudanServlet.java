077690c58ceceaa57b8c5cd177ab8a67ab0f1f80
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-21 13:20:53 +0900

    GugudanServlet 추가, 동적 웹 구현

---
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -0,0 +1,28 @@
+package com.ll.exam;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=utf-8");
+
+        int dan = Integer.parseInt(req.getParameter("dan"));
+        int limit = Integer.parseInt(req.getParameter("limit"));
+
+        resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+
+        for (int i = 1; i <= limit; i++) {
+            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+        }
+    }
+}
